586ff8433618a41f04dc102deaed81e443f0bfd5
shivamgr8 <shivam@gr8logics.com>
2023-07-29 22:08:33 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App(): React.ReactElement {
     return (
         <div className="App">
-            <h1>App.tsx</h1>
+            <h1>First commit</h1>
         </div>
     );
 }
